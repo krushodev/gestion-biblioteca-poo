b1e8a310d4a73b4d04599ef9794348e68390249d
Ignacio Kruchowski <ignakruchowski@gmail.com>
2025-10-30 20:50:01 -0300

    first commit

---
+++ gestion-biblioteca/Libro.java
@@ -0,0 +1,57 @@
+public class Libro {
+    private String titulo;
+    private int edicion;
+    private String editorial;
+    private int anio;
+
+    public Libro(String p_titulo, int p_edicion, String p_editorial, int p_anio) {
+        this.setTitulo(p_titulo);
+        this.setEdicion(p_edicion);
+        this.setEditorial(p_editorial);
+        this.setAnio(p_anio);
+    }
+
+    private void setTitulo(String p_titulo) {
+        this.titulo = p_titulo;
+    }
+
+    private void setEdicion(int p_edicion) {
+        this.edicion = p_edicion;
+    }
+
+    private void setEditorial(String p_editorial) {
+        this.editorial = p_editorial;
+    }
+
+    private void setAnio(int p_anio) {
+        this.anio = p_anio;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getEdicion() {
+        return edicion;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public boolean prestado() {
+        return false;
+    }
+
+    public Prestamo prestamo() {
+        return new Prestamo();
+    }
+
+    public String toString() {
+        return "Titulo: " + titulo;
+    }
+}
