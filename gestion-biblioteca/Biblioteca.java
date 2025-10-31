fa8645f8c62301ada4340560e78cc92b6294dbe7
Ignacio Kruchowski <ignakruchowski@gmail.com>
2025-10-30 22:01:42 -0300

    feat: implement Biblioteca methods

--- gestion-biblioteca/Biblioteca.java
+++ gestion-biblioteca/Biblioteca.java
@@ -1,33 +1,93 @@
+import java.util.ArrayList;
+import java.util.Calendar;
 
-/**
- * Write a description of class Biblioteca here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Biblioteca
-{
-    // instance variables - replace the example below with your own
-    private int x;
+public class Biblioteca {
+    private String nombre;
+    private ArrayList<Libro> libros;
+    private ArrayList<Socio> socios;
 
-    /**
-     * Constructor for objects of class Biblioteca
-     */
-    public Biblioteca()
-    {
-        // initialise instance variables
-        x = 0;
+    public Biblioteca(String p_nombre) {
+        this.setNombre(p_nombre);
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    private void setNombre(String p_nombre) {
+        this.nombre = p_nombre;
     }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public ArrayList<Libro> getLibros() {
+        return this.libros;
+    }
+
+    public ArrayList<Socio> getSocios() {
+        return this.socios;
+    }
+
+    private void agregarLibro(Libro p_libro) {
+        this.getLibros().add(p_libro);
+    }
+
+    public void nuevoLibro(String p_titulo, int p_edicion, String p_editorial, int p_anio) {
+        this.agregarLibro(new Libro(p_titulo, p_edicion, p_editorial, p_anio));
+    }
+
+    /*
+     * public void nuevoSocioEstudiante(int p_dniSocio, String p_nombre, String
+     * p_carrera) {
+     * this.getSocios().add(new Estudiante(p_dniSocio, p_nombre, p_carrera));
+     * }
+     * 
+     * public void nuevoSocioDocente(int p_dniSocio, String p_nombre, String p_area)
+     * {
+     * this.getSocios().add(new Docente(p_dniSocio, p_nombre, p_area));
+     * }
+     */
+
+    public boolean prestarLibro(Calendar p_fechaRetiro, Socio p_socio, Libro p_libro) {
+        return true;
+    }
+
+    public void devolverLibro(Libro p_libro) {
+        return;
+    }
+
+    public int cantidadDeSociosPorTipo(String p_objeto) {
+        return 0;
+    }
+
+    public ArrayList<Prestamo> prestamosVencidos() {
+        return new ArrayList<Prestamo>();
+    }
+
+    public ArrayList<Docente> docentesResponsables() {
+        return new ArrayList<Docente>();
+    }
+
+    public String quienTieneElLibro(Libro p_libro) {
+        return "";
+    }
+
+    public String listaDeSocios() {
+        return "";
+    }
+
+    public Socio buscarSocio(int p_dni) {
+        return null;
+    }
+
+    public String listaDeTitulos() {
+        return "";
+    }
+
+    public String listaDeLibros() {
+        return "";
+    }
+
+    public String listaDeDocentesResponsables() {
+        return "";
+    }
+
 }
